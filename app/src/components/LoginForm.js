834a7a0be3ac2de85df15888807382e99345fe4a
j-greenberg <jgreenberg@runbox.com>
2020-04-29 11:03:31 -0400

    updates

--- app/src/components/LoginForm.js
+++ app/src/components/LoginForm.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
+import axios from 'axios'; 
 
 export default function LoginForm () {
 
-    const [user, setUser] = useState({ username: '', email: '', password: '' })
+    const [login, setLogin] = useState({ username: '', password: '' })
+    const [register, setRegister] = useState({ name: '', username: '', email: '', password: '' })
+    let token = ''; 
 
-    function handleChange(e){
-        setUser({...user, [e.target.name]:e.target.value})
-        console.log(user); 
+    function handleRegisterChange(event){
+        setRegister({...register, [event.target.name]:event.target.value}); 
+        console.log(register); 
     }
 
-    function handleSubmit(){
+    function handleLoginChange(event){
+        setLogin({...login, [event.target.name]:event.target.value}); 
+        console.log(login); 
+    }
 
+    function handleRegister(event){
+        event.preventDefault(); 
+        axios.post('https://post-here-subreddit.herokuapp.com/api/auth/register', register)
+            .then(response => {
+                console.log("Successfully registered!: ", response.data.response); 
+            })
+            .catch(error => {
+                console.log(error); 
+            })
+    }
+
+    function handleLogin(event){
+        event.preventDefault(); 
+        axios.post('https://post-here-subreddit.herokuapp.com/api/auth/login', login)
+            .then(response => {
+                console.log(response); 
+                token = response.data.token; 
+                console.log('Token: ', token); 
+            })
+            .catch(error => {
+                console.log(error); 
+            })
     }
 
     return (
@@ -18,20 +46,22 @@ export default function LoginForm () {
             <form>
                 <h1>Welcome to Post Here Subreddit!</h1>
                 <h2>Register below:</h2>
-                <label>Username: </label>
-                <input name="username" onChange={handleChange} value={user.username}/><br></br>
-                <label>Email: </label>
-                <input name="email" onChange={handleChange} value={user.email}/><br></br>
-                <label>Password: </label>
-                <input name="password" onChange={handleChange} value={user.password}/><br></br>
-                <button onSubmit={handleSubmit}>Register</button>
+                <label>Name: </label>
+                <input name="name" onChange={handleRegisterChange} value={register.name}/><br></br>
+                <label>Username (*required): </label>
+                <input name="username" onChange={handleRegisterChange} value={register.username}/><br></br>
+                <label>Email (*required): </label>
+                <input name="email" onChange={handleRegisterChange} value={register.email}/><br></br>
+                <label>Password (*required): </label>
+                <input name="password" onChange={handleRegisterChange} value={register.password}/><br></br>
+                <button onClick={handleRegister}>REGISTER</button>
                 <br></br><br></br>
                 OR<br></br><br></br><br></br>
                 Login below: 
-                <input name="username" onChange={handleChange} value={user.username}/><br></br>
+                <input name="username" onChange={handleLoginChange} value={login.username}/><br></br>
                 <label>Password: </label>
-                <input name="password" onChange={handleChange} value={user.password}/><br></br>
-                <button onSubmit={handleSubmit}>Login</button>
+                <input name="password" onChange={handleLoginChange} value={login.password}/><br></br>
+                <button onClick={handleLogin}>LOGIN</button>
             </form>
         </div>
     )
